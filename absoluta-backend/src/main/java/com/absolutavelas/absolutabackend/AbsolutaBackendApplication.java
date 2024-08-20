df393783e191b7cdd50ed6bf035eade87c5ec57c
Douglas Bello <dgbello.dev@hotmail.com>
2024-08-20 11:40:00 -0300

    feat(backend): basic structure

---
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/AbsolutaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.absolutavelas.absolutabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsolutaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsolutaBackendApplication.class, args);
+    }
+
+}
---
+++ absoluta-backend/src/test/java/com/absolutavelas/absolutabackend/AbsolutaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absolutavelas.absolutabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsolutaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
