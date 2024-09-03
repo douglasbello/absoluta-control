07c206a7e33afdc9868b777f648898cad602f8c4
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-03 16:16:27 -0300

    docs: marketplace type register controllers

--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/marketplaces/types/MarketplaceTypeRegisterController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/marketplaces/types/MarketplaceTypeRegisterController.java
@@ -2,8 +2,17 @@ package com.absolutavelas.absolutabackend.controllers.marketplaces.types;
 
 import com.absolutavelas.absolutabackend.database.models.marketplaces.MarketplaceType;
 import com.absolutavelas.absolutabackend.dtos.marketplaces.types.MarketplaceTypeRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Marketplace type register endpoints.")
 public interface MarketplaceTypeRegisterController {
+    @Operation(summary = "Registers Marketplace type.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Returns Marketplace type in body and the URI in header.")
+    })
     ResponseEntity<MarketplaceType> register(MarketplaceTypeRequest request);
 }
--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/marketplaces/types/MarketplaceTypeSearchController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/marketplaces/types/MarketplaceTypeSearchController.java
@@ -1,13 +1,36 @@
 package com.absolutavelas.absolutabackend.controllers.marketplaces.types;
 
 import com.absolutavelas.absolutabackend.database.models.marketplaces.MarketplaceType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Marketplace type endpoints.")
 public interface MarketplaceTypeSearchController {
+    @Operation(summary = "Find Marketplace type by identifier")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns Marketplace type found by identifier."),
+            @ApiResponse(responseCode = "404", description = "Marketplace type not found."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<MarketplaceType> findByIdentifier(UUID identifier);
+
+    @Operation(summary = "Finds all Marketplace types.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns all Marketplace types."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<List<MarketplaceType>> findAll();
+
+    @Operation(summary = "Finds all Marketplace types by type name.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns all Marketplace types found by type name."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<List<MarketplaceType>> findByTypeName(String type);
 }
