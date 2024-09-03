0fc9492273efab7dd2868af5eca79c6c6c587e74
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-03 14:56:29 -0300

    docs: product size controllers

--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/products/size/ProductSizeRegisterController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/products/size/ProductSizeRegisterController.java
@@ -2,8 +2,18 @@ package com.absolutavelas.absolutabackend.controllers.products.size;
 
 import com.absolutavelas.absolutabackend.database.models.products.ProductSize;
 import com.absolutavelas.absolutabackend.dtos.products.size.ProductSizeRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Product size register controller")
 public interface ProductSizeRegisterController {
+    @Operation(summary = "Register a product.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Product size registered successfully. Returns the Product size in body and the URI in headers."),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error.")
+    })
     ResponseEntity<ProductSize> register(ProductSizeRequest request);
 }
--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/products/size/ProductSizeSearchController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/products/size/ProductSizeSearchController.java
@@ -1,12 +1,29 @@
 package com.absolutavelas.absolutabackend.controllers.products.size;
 
 import com.absolutavelas.absolutabackend.database.models.products.ProductSize;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Product size search controller")
 public interface ProductSizeSearchController {
+    @Operation(summary = "Find a product size by it's identifier.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns the product size"),
+            @ApiResponse(responseCode = "404", description = "Product size not found."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<ProductSize> findByIdentifier(UUID identifier);
+
+    @Operation(summary = "Find all product sizes.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns all product sizes in database."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<List<ProductSize>> findAll();
 }
