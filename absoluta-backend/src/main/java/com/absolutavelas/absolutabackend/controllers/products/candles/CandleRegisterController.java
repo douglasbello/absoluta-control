d9ad35d0f739e588d5f95b53155198b7bb42b3f0
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-03 15:23:59 -0300

    docs: candle register and search controllers

--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/products/candles/CandleRegisterController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/products/candles/CandleRegisterController.java
@@ -2,8 +2,18 @@ package com.absolutavelas.absolutabackend.controllers.products.candles;
 
 import com.absolutavelas.absolutabackend.database.models.products.Candle;
 import com.absolutavelas.absolutabackend.dtos.products.candles.CandleRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Candle register endpoints.")
 public interface CandleRegisterController {
+    @Operation(summary = "Registers candle.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Candle registered and returned in body, URI returned in the headers."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<Candle> register(CandleRequest request);
 }
--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/products/candles/CandleSearchController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/products/candles/CandleSearchController.java
@@ -1,16 +1,51 @@
 package com.absolutavelas.absolutabackend.controllers.products.candles;
 
 import com.absolutavelas.absolutabackend.database.models.products.Candle;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 import java.util.UUID;
 
+@Tag(name = "Candle search endpoints.")
 public interface CandleSearchController {
+    @Operation(summary = "Find candle by identifier.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns found candle."),
+            @ApiResponse(responseCode = "404", description = "Candle not found."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<Candle> findByIdentifier(UUID identifier);
+
+    @Operation(summary = "Finds paginated amount of candles.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns paginated amount of candles."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<Page<Candle>> findAll(Pageable pageable);
+
+    @Operation(summary = "Finds paginated amount of candles ordered by stock amount ascending.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns paginated amount of candles ordered by stock amount ascending."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<Page<Candle>> allOrderedByStockAmountAsc(Pageable pageable);
+
+    @Operation(summary = "Finds paginated amount of candles ordered by stock amount descending.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns paginated amount of candles ordered by stock amount descending."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<Page<Candle>> allOrderedByStockAmountDesc(Pageable pageable);
+
+    @Operation(summary = "Finds the candles that sells the most.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns all most sold candles."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<Page<Candle>> bestSellers(Pageable pageable);
 }
