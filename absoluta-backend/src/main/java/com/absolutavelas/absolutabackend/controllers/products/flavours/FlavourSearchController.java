c9185deeb1bba5ffdac69144c1bae8801710ca27
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-05 10:42:57 -0300

    docs: sorting tags alphabeticaly

--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/products/flavours/FlavourSearchController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/products/flavours/FlavourSearchController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import java.util.List;
 import java.util.UUID;
 
-@Tag(name = "Products flavours endpoint.")
+@Tag(name = "Flavours search endpoints.")
 public interface FlavourSearchController<T> {
     @Operation(summary = "Finds flavour by identifier.")
     @ApiResponses(value = {
