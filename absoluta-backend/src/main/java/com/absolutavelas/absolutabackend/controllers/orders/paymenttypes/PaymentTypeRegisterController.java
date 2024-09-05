e1726788c928f25751a539d1afaf7c5d945d378e
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-05 09:26:07 -0300

    docs: payment type register and search controllers

--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/orders/paymenttypes/PaymentTypeRegisterController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/orders/paymenttypes/PaymentTypeRegisterController.java
@@ -2,8 +2,18 @@ package com.absolutavelas.absolutabackend.controllers.orders.paymenttypes;
 
 import com.absolutavelas.absolutabackend.database.models.orders.PaymentType;
 import com.absolutavelas.absolutabackend.dtos.orders.PaymentTypeRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Payment type register endpoints.")
 public interface PaymentTypeRegisterController {
+    @Operation(summary = "Register a payment type.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Registered successfully, entity returned in body and URI returned in header."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<PaymentType> register(PaymentTypeRequest request);
 }
--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/orders/paymenttypes/PaymentTypeSearchController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/orders/paymenttypes/PaymentTypeSearchController.java
@@ -1,12 +1,29 @@
 package com.absolutavelas.absolutabackend.controllers.orders.paymenttypes;
 
 import com.absolutavelas.absolutabackend.database.models.orders.PaymentType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Payment type search endpoints.")
 public interface PaymentTypeSearchController {
+    @Operation(summary = "Find payment type by identifier.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return found payment type."),
+            @ApiResponse(responseCode = "404", description = "Payment type not found."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
+
     ResponseEntity<PaymentType> findByIdentifier(UUID identifier);
+    @Operation(summary = "Finds all payment types.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns all payment types."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<List<PaymentType>> findAll();
 }
