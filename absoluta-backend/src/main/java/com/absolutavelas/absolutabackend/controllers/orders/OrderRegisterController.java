035c8e0096b0100ebfa1ea9677ec14341d1d043b
Douglas Bello <dgbello.dev@hotmail.com>
2024-09-05 09:43:37 -0300

    docs: order register and search controllers

--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/orders/OrderRegisterController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/orders/OrderRegisterController.java
@@ -2,8 +2,18 @@ package com.absolutavelas.absolutabackend.controllers.orders;
 
 import com.absolutavelas.absolutabackend.database.models.orders.Order;
 import com.absolutavelas.absolutabackend.dtos.orders.OrderRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Order register endpoints.")
 public interface OrderRegisterController {
+    @Operation(summary = "Register a order.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Returns entity in body and URI in header."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<Order> register(OrderRequest request);
 }
--- absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/orders/OrderSearchController.java
+++ absoluta-backend/src/main/java/com/absolutavelas/absolutabackend/controllers/orders/OrderSearchController.java
@@ -1,13 +1,30 @@
 package com.absolutavelas.absolutabackend.controllers.orders;
 
 import com.absolutavelas.absolutabackend.database.models.orders.Order;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 import java.util.UUID;
 
+@Tag(name = "Order search endpoints.")
 public interface OrderSearchController {
+    @Operation(summary = "Find order by identifier.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return order found."),
+            @ApiResponse(responseCode = "404", description = "Order not found."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<Order> findByIdentifier(UUID identifier);
+
+    @Operation(summary = "Finds all paginated orders.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Return orders paginated."),
+            @ApiResponse(responseCode = "500", description = "Internal server error.")
+    })
     ResponseEntity<Page<Order>> findAll(Pageable pageable);
 }
