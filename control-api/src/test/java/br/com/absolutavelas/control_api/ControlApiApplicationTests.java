169ae66e7c2a568e51052dd4fff6f09b5a48d2ae
Douglas Bello <dgbello.dev@hotmail.com>
2024-10-03 10:57:23 -0300

    feat: control-api, this will be the new backend, absoluta-backend deprecated

---
+++ control-api/src/test/java/br/com/absolutavelas/control_api/ControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.absolutavelas.control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
